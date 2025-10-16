931e0d95af4fdb1ceff4a96fff127dd275bbdf2b
shuy697965 <shuy1152880304@gmail.com>
2025-10-16 08:57:21 +0800

    Update userID and clear proxyIP variable

--- zizifn原始代码.js
+++ zizifn原始代码.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd15c90c8-56a6-4390-af6c-77ef8b06de43';
+let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-let proxyIP = 'bpb.yousef.isegaro.com';
+let proxyIP = '';
 
 
 if (!isValidUUID(userID)) {
@@ -633,4 +633,3 @@ clash-meta
 ################################################################
 `;
 }
-
